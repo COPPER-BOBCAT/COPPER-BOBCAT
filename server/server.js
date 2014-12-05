9d8c8f5fed4edad30ddd0c5061ffa8bf986361c7
Issac <issac@Issacs-MacBook-Air.local>
2014-12-04 16:33:59 -0800

    put in basic template routes and controllers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 var express = require('express');
-//var db = require('./db');
+// var db = require('./db');
 var app = express();
+var router = require('./routes.js');
 var config = require('./oauth.js')
 var passport = require('passport')
 var GoogleStrategy = require('passport-google').Strategy;
 
-
+app.use("/questions", router);
 
 var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '../../public'));
